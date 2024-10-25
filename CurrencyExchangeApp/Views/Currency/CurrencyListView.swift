49616047f58fb57ee526061df7cca3c92a9ea1e0
kurtymd <kurtimdmitruk@gmail.com>
2024-10-25 15:26:54 +0200

    fix: adjust the structure to ensure that the button behaves as expected with buttonStyle(.plain) make sure that Spacer() is tappable

--- CurrencyExchangeApp/Views/Currency/CurrencyListView.swift
+++ CurrencyExchangeApp/Views/Currency/CurrencyListView.swift
@@ -9,24 +9,37 @@ import SwiftUI
 
 struct CurrencyListView: View {
     @StateObject private var viewModel = CurrencyViewModel()
+    @State private var selectedRate: Rate? = nil
     
     var body: some View {
         NavigationStack {
             List(viewModel.rates, id: \.code) { rate in
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(rate.code)
-                            .font(.title2)
-                            .bold()
-                        Text(rate.currency.capitalized)
-                            .foregroundStyle(Color(.secondaryLabel))
-                            .font(.caption)
+                Button {
+                    selectedRate = rate
+                    print("Selected Rate: \(String(describing: selectedRate))")
+                } label: {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(rate.code)
+                                .foregroundStyle(Color.primary)
+                                .font(.title2)
+                                .bold()
+                            Text(rate.currency.capitalized)
+                                .foregroundStyle(Color(.secondaryLabel))
+                                .font(.caption)
+                        }
+                        Spacer()
+                        VStack {
+                            Text(String(format: "%.4f", rate.mid) + "zł")
+                                .foregroundStyle(Color.primary)
+                                .font(.headline)
+                        }
                     }
-                    Spacer()
-                    VStack {
-                        Text(String(format: "%.4f", rate.mid) + "zł")
-                            .font(.headline)
-                    }
+                    .contentShape(Rectangle())
+                }
+                .buttonStyle(.plain)
+                .sheet(item: $selectedRate) { rate in
+                    ItemSheetView(rate: rate)
                 }
             }
             .navigationTitle("Currencies")
