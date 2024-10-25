bc70def4d1c39b4cbb10627a428d931e50387bd4
kurtymd <kurtimdmitruk@gmail.com>
2024-10-25 12:03:22 +0200

    feat: add ItemSheetView and ListItemView as Components for CurrencyListView

---
+++ CurrencyExchangeApp/Views/Currency/Components/ItemSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ItemSheetView.swift
+//  CurrencyExchangeApp
+//
+//  Created by Bohdan Dmytruk on 25/10/2024.
+//
+
+import SwiftUI
+
+struct ItemSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ItemSheetView()
+}
---
+++ CurrencyExchangeApp/Views/Currency/Components/ListItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListItemView.swift
+//  CurrencyExchangeApp
+//
+//  Created by Bohdan Dmytruk on 25/10/2024.
+//
+
+import SwiftUI
+
+struct ListItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListItemView()
+}
