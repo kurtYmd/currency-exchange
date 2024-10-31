8b971c0c0670c315a056f88291d1fb46195cc140
kurtymd <kurtimdmitruk@gmail.com>
2024-10-31 11:57:40 +0100

    refactor: Replace SellView and BuyView with consolidated ExchangeView
    - Combined SellView and BuyView functionality into a single ExchangeView

---
+++ CurrencyExchangeApp/Views/Currency/Components/ExchangeSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  ExchangeSheetView.swift
+//  CurrencyExchangeApp
+//
+//  Created by Bohdan Dmytruk on 29/10/2024.
+//
+
+import SwiftUI
+
+struct ExchangeSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExchangeSheetView()
+}
