a03b6eb6c71024c3c35c3460da7c60329258c544
kurtymd <kurtimdmitruk@gmail.com>
2024-11-14 16:36:53 +0100

    Refactor Transaction model to support "Top Up" type of transaction

--- CurrencyExchangeApp/Models/Transaction.swift
+++ CurrencyExchangeApp/Models/Transaction.swift
@@ -9,19 +9,28 @@ import Foundation
 import FirebaseFirestore
 
 struct Transaction: Codable {
-    let currencyFrom: String
-    let currencyTo: String
+    let currencyFrom: String?
+    let currencyTo: String?
     let amount: Double
     let type: TransactionType
     let date: Date
     
+    init(currencyFrom: String? = nil, currencyTo: String? = nil, amount: Double, type: TransactionType, date: Date) {
+        self.currencyFrom = currencyFrom
+        self.currencyTo = currencyTo
+        self.amount = amount
+        self.type = type
+        self.date = date
+    }
+    
     func toDictionary() -> [String: Any] {
-            return [
-                "currencyFrom": currencyFrom,
-                "currencyTo": currencyTo,
-                "amount": amount,
-                "type": type,
-                "date": Timestamp(date: date) // Firestore uses Timestamp for dates
-            ]
-        }
+        var dict: [String: Any] = [
+            "amount": amount,
+            "type": type.rawValue,
+            "date": Timestamp(date: date)
+        ]
+        if let currencyFrom = currencyFrom {dict["currencyFrom"] = currencyFrom }
+        if let currencyTo = currencyTo { dict["currencyTo"] = currencyTo }
+        return dict
+    }
 }
